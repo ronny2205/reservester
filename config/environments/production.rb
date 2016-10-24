48957d7d2f1ae139e7fd435482bd2c7db44299b8
Ronny Almog <ronny2205@gmail.com>
2016-10-24 18:31:13 +0000

    update saas-rails gem version

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
    For devise gem
   config.action_mailer.default_url_options = { host: 'https://reservester-ronny.herokuapp.com', port: 3000 }
