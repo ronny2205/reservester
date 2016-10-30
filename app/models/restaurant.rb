16a18395b94302830f76a0b1e991a858ce3bda94
Ronny Almog <ronny2205@gmail.com>
2016-10-30 23:55:56 +0000

    validations and tests

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,4 @@
 class Restaurant < ApplicationRecord
 	belongs_to :user
+	validates :name, :address, :phone, :presence => true
 end
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+   it 'validates the presence of name' do
+    restaurant = Restaurant.new
+    restaurant.valid?
+    expect(restaurant.errors.messages[:name]).to include "can't be blank"
+  end
 end
