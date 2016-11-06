76a4da3da2caea52cf63ded47aefaa84e287168c
Ronny Almog <ronny2205@gmail.com>
2016-11-06 22:06:25 +0000

    adding reservation mailer

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,9 @@
 class Reservation < ApplicationRecord
 	belongs_to :restaurant
 	validates :email, :date, :time, :presence => true
+	after_create :send_reservation_email
+
+	def send_reservation_email
+      ReservationMailer.reservation_notification(self).deliver
+  end
 end
